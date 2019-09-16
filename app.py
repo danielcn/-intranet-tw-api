2b75c15bd2a44d481b58413b9513107715c5679e
Daniel Nascimento <danascimen@thoughtworks.com>
2019-09-16 11:23:28 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return 'Jobs and jobs for all'
+
+@app.route('/templage_job')
+def templage_job():
+  return 'Endpoint for template job'
+  
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
